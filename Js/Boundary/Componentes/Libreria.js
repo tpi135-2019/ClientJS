5c08cdd04c69147b31f096f9dee9628634da34b7
Kevin Chinchilla <kchinchillam98@gmail.com>
2019-06-04 02:21:34 -0600

    automulti completo

--- Js/Boundary/Componentes/Libreria.js
+++ Js/Boundary/Componentes/Libreria.js
@@ -1,3 +1,4 @@
 import TPITabla from './TPITabla.js';
 import TPIAutocomplete from './TPIAutocomplete.js';
-
+import TPIAutocompleteMulti from './TPIAutocompleteMulti.js';
+import TPICheckbox from './TPICheckbox.js';
--- Js/Boundary/frmTabla.js
+++ Js/Boundary/frmTabla.js
@@ -3,29 +3,47 @@ import MetodsT from '../Control/MetodsT.js';
 const controlador = new MetodsT();
 
 let tabla = document.querySelector("#idtabla");
-let first= tabla.getAttribute("first");
-let pageSize= tabla.getAttribute("pagesize");
+let first = tabla.getAttribute("first");
+let pageSize = tabla.getAttribute("pagesize");
 
-controlador.find(`marca?first=${first}&pagesize=${pageSize}`).then((data)=> {
+controlador.find(`marca?first=${first}&pagesize=${pageSize}`).then((data) => {
     let tabla = document.querySelector("#idtabla");
-    tabla.setAttribute("datos",JSON.stringify(data));
+    tabla.setAttribute("datos", JSON.stringify(data));
 });
 
-document.addEventListener("Click_btn", e=> {
+document.addEventListener("Click_btn", e => {
     llenado();
 });
 
-document.addEventListener('cambio-opcion',evt => {
+document.addEventListener('cambio-opcion', evt => {
     llenado();
 });
 
-function llenado(){
+function llenado() {
 
-    let first= tabla.getAttribute("first");
-    let pageSize= tabla.getAttribute("pagesize");
+    let first = tabla.getAttribute("first");
+    let pageSize = tabla.getAttribute("pagesize");
     //`?first=${first}&pagesize=${pageSize}`
-      controlador.find(`marca?first=${first}&pagesize=${pageSize}`).then((data)=> {
+    controlador.find(`marca?first=${first}&pagesize=${pageSize}`).then((data) => {
         let tabla = document.querySelector("#idtabla");
-        tabla.setAttribute("datos",JSON.stringify(data));
+        tabla.setAttribute("datos", JSON.stringify(data));
     });
 }
+
+
+let section = document.querySelector("#multi");
+section.addEventListener('eventfilter', e => {
+    let filtro = e.detail.filtro;
+    controlador.find(`proceso/1/pasos?paso=${filtro}`).
+    then(data => {
+        let mapaPasos = new Map();
+        if (data !== null) {
+            data.forEach(dato => {
+                mapaPasos.set(dato.idPasoProceso, dato.idPaso.nombre);
+            })
+        }
+        const evento = new CustomEvent("datachange", { composed: true, detail: { response: mapaPasos }, bubbles: true });
+        e.target.dispatchEvent(evento);
+
+    });
+});
